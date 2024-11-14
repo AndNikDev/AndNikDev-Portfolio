339a437a03332667fe30b87eb63842cc7f73a323
Nikolayk Muñoz A. <andnik.developer@gmail.com>
2024-11-14 15:15:52 -0600

    diff changes

---
+++ src/components/Projects_Carousel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects_Carousel = () => {
+  return <div></div>;
+};
+
+export default Projects_Carousel;
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,5 +1,11 @@
+import "../assets/styles/Experience.css";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="experience__section">
+      <h1 className="experience__title">Experience</h1>
+    </div>
+  );
 };
 
 export default Experience;
